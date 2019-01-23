5612669ca54c5dedfd31674f77b12a3c83c637a4
Ben Haeringer <ben.haeringer@gmail.com>
2019-01-23 12:57:46 +0100

    improve output

--- bmtsmb.sh
+++ bmtsmb.sh
@@ -5,7 +5,9 @@
 # Description : Sync local directory on MacOS with SMB share. Auto-manage the
 #               mount depending on network connectivity to share.
 # Usage       : Run with cron, e.g. once every 5 minutes:
-#               */5 * * * * sh bmtsmb.sh 2>/dev/null
+#               */5 * * * * ./bmtsmb.sh 2>/dev/null
+#               Write to log file with timestamp:
+#               ./bmtsmb.sh | while IFS= read -r line; do echo "$(date)  $line"; done >>/tmp/bmtsmb.log
 # Author      : Benjamin Haeringer
 # Email       : ben.haeringer@gmail.com
 # Date        : 2018/03/02
@@ -17,23 +19,24 @@ mntpnt="/Users/username/smbshare"
 share="//username@smbserver/user-share-dir"
 server="smbserver"
 
+echo "Starting backup script: check mount & share server status"
 
 # check if share is mounted
 if /sbin/mount | grep "$share" > /dev/null; then
     mounted=1
-    echo "share is currently mounted"
+    echo "Share is mounted"
 else
     mounted=0
-    echo "share is currently not mounted"
+    echo "Share is not mounted"
 fi
 
 # check if share server is reachable via smb
 if /usr/bin/nc -z "$server" 445 &>/dev/null; then
     srv_ok=1
-    echo "share server reachable via smb"
+    echo "Share server reachable via smb"
 else
     srv_ok=0
-    echo "share server not reachable"
+    echo "Share server not reachable"
 fi
 
 # mount, unmount or do nothing
@@ -47,17 +50,17 @@ elif [ $srv_ok == 1 ] && [ $mounted == 0 ]; then
         echo "creating mountpoint directory"
         /bin/mkdir "$mntpnt"
     fi
-    echo "mounting share"
+    echo "Mounting share"
     if /sbin/mount_smbfs "$share" "$mntpnt"; then
         while [ ! -d "$mntpnt$backupdir" ]; do
-            echo "waiting for mount to be ready..."
+            echo "Waiting for mount to be ready..."
             sleep 2
         done
-        echo "mount ready"
+        echo "Mount ready"
         ready=1
     else
-        echo "mount failed; quit"
-        exit 0
+        echo "Mount failed"
+        ready=0
     fi
 elif [ $srv_ok == 1 ] && [ $mounted == 1 ]; then
     ready=1
@@ -66,16 +69,16 @@ fi
 
 # run backup, depending on mount- and rsync job status
 if [ $ready == 0 ]; then
-    echo "backup not possible; quit"
+    echo "Backup not possible; quit"
     exit 0
 elif [ $ready == 1 ]; then
     if /usr/bin/pgrep -f /usr/local/bin/rsync.*"$mntpnt" > /dev/null; then
-        echo "previous backup job still running; quit"
+        echo "Previous backup job still running; quit"
         exit 0
     else
-        echo "running rsync backup..."
+        echo "Running rsync backup..."
        /usr/local/bin/rsync -a --delete --ignore-errors "$HOME$backupdir" "$mntpnt"/
-        echo "done"
+        echo "Done"
     fi
 else exit 2
 fi
